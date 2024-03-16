a4aeeaecaa0ce6b93fb2f5bd57e743b26c27608b
HarryEP <44836842+HarryEP@users.noreply.github.com>
2024-03-16 22:23:21 +0000

    first test

--- test_main.py
+++ test_main.py
@@ -1,1 +1,8 @@
 '''this is the test file for main.py'''
+from main import clean_data
+
+
+def test_clean_data_removes_empty_lines():
+    lines = ['hello', '', '', 'how are you']
+    all_lines = clean_data(lines)
+    assert all_lines == ['hello', 'how are you']
